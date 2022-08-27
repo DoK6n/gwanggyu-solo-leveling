6eae80f10e2337513b70cebfe3f53882673dc8fb
Dok6n <ehrbs2018@gmail.com>
2022-08-27 19:14:39 +0900

    feat: add App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, React</h1>
+    <App />
   </React.StrictMode>
 );
