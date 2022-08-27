9f48c33adb17553a88624ec55d6de28f4ebda559
Dok6n <ehrbs2018@gmail.com>
2022-08-28 00:59:06 +0900

    feat: add tailwind style button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function App() {
-  return <div>Hello, React</div>;
+  return (
+    <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-teal-600 hover:bg-teal-800 active:bg-teal-900">
+      Solo Leveling
+    </button>
+  );
 }
