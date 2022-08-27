26a2a190bbad3f458e792b3afc12788ceaa7b392
Dok6n <ehrbs2018@gmail.com>
2022-08-28 00:57:24 +0900

    chore(conf): add theme color options

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: true,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#1fb6ff',
+        purple: '#7e5bef',
+        pink: '#ff49db',
+        orange: '#ff7849',
+        green: '#13ce66',
+        yellow: '#ffc82c',
+        'gray-dark': '#273444',
+        gray: '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+    },
   },
   plugins: [],
 };
