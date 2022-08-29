3a856ee32fc2ca366cabbcdab94e52ce24cb493e
Dok6n <ehrbs2018@gmail.com>
2022-08-29 14:22:28 +0900

    feat: add props

--- src/components/status/playerHp.tsx
+++ src/components/status/playerHp.tsx
@@ -3,12 +3,16 @@ import { playerDB } from '../../database';
 import { useTotalOperatingRevenue } from '../../hooks';
 import { Category } from './category';
 
-export const PlayerHp = () => {
+interface PlayerHpProps {
+  totalOperatingRevenue: number;
+}
+
+export const PlayerHp = ({ totalOperatingRevenue }: PlayerHpProps) => {
   return (
     <>
       <Category categoryKey="타이틀" categoryValue={'본업과 부업이 바뀐자'} />
       <div>
-        <span className="text-red-500">HP</span>: {useTotalOperatingRevenue(playerDB)} / 150
+        <span className="text-red-500">HP</span>: {totalOperatingRevenue} / 150
       </div>
     </>
   );
