7d283ae3a285771ab23bb9a99c011472b36aa3ea
Dok6n <ehrbs2018@gmail.com>
2022-08-29 10:58:17 +0900

    feat: add level criteria constant

---
+++ src/constants/levelcriteria.ts
@@ -0,0 +1,17 @@
+export const LEVEL_CRITERIA = {
+  F: 'F',
+  'F+': 'F+',
+  E: 'E',
+  'E+': 'E+',
+  D: 'D',
+  'D+': 'D+',
+  C: 'C',
+  'C+': 'C+',
+  B: 'B',
+  'B+': 'B+',
+  A: 'A',
+  'A+': 'A+',
+  S: 'S',
+  SS: 'SS',
+  SSS: 'SSS',
+} as const;
