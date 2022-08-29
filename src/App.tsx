19d832b0b57118941447292589e07f8a071c60af
Dok6n <ehrbs2018@gmail.com>
2022-08-29 14:23:06 +0900

    feat: update player db

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Screen, PlayerInfo, PlayerCall, Category, PlayerHp, Image } from './components';
-import dayjs from 'dayjs';
-import { playerDB } from './database';
-import { useLevelFinder, useTotalOperatingRevenue } from './hooks';
+import { currentMonthPlayerDB, totalOperatingRevenue, playerLevel } from './database';
 
 export default function App() {
-  const [currentLevel, setCurrentLevel] = useState<string>(
-    String(useLevelFinder(useTotalOperatingRevenue(playerDB))),
-  );
   return (
     <main className="container min-h-screen max-w-full p-1">
       <section className="flex flex-col items-center p-4 m-2">
-        <Image level={currentLevel} />
+        <Image level={playerLevel} />
         <Screen>
           <PlayerInfo>
             <Category categoryKey="이름" categoryValue={'이광규'} />
-            <Category categoryKey="등급" categoryValue={currentLevel} />
+            <Category categoryKey="등급" categoryValue={playerLevel} />
           </PlayerInfo>
           <hr className="my-4" />
-          <PlayerHp />
+          <PlayerHp totalOperatingRevenue={totalOperatingRevenue} />
           <hr className="my-4" />
-          {playerDB
-            .sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf())
-            .map(db => (
-              <PlayerCall
-                commission={db.commission}
-                date={db.date}
-                downPayment={db.downPayment}
-                operatingRevenue={db.operatingRevenue}
-                key={db.id}
-              />
-            ))}
+          {currentMonthPlayerDB.map(db => (
+            <PlayerCall
+              commission={db.commission}
+              date={db.date}
+              downPayment={db.downPayment}
+              operatingRevenue={db.operatingRevenue}
+              key={db.id}
+            />
+          ))}
         </Screen>
       </section>
     </main>
