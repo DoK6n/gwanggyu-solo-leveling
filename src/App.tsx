4a604cf997048f1f283f8d4671f2b8113f2beebd
Dok6n <ehrbs2018@gmail.com>
2022-08-29 11:02:17 +0900

    feat: add player status screen component & level image in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Screen, PlayerInfo, PlayerCall, Category, PlayerHp, Image } from './components';
+import dayjs from 'dayjs';
+import { playerDB } from './database';
+import { useLevelFinder, useTotalOperatingRevenue } from './hooks';
 
 export default function App() {
+  const [currentLevel, setCurrentLevel] = useState<string>(
+    String(useLevelFinder(useTotalOperatingRevenue(playerDB))),
+  );
   return (
-    <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-teal-600 hover:bg-teal-800 active:bg-teal-900">
-      Solo Leveling
-    </button>
+    <main className="container min-h-screen max-w-full p-1">
+      <section className="flex flex-col items-center p-4 m-2">
+        <Image level={currentLevel} />
+        <Screen>
+          <PlayerInfo>
+            <Category categoryKey="이름" categoryValue={'이광규'} />
+            <Category categoryKey="등급" categoryValue={currentLevel} />
+          </PlayerInfo>
+          <hr className="my-4" />
+          <PlayerHp />
+          <hr className="my-4" />
+          {playerDB
+            .sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf())
+            .map(db => (
+              <PlayerCall
+                commission={db.commission}
+                date={db.date}
+                downPayment={db.downPayment}
+                operatingRevenue={db.operatingRevenue}
+                key={db.id}
+              />
+            ))}
+        </Screen>
+      </section>
+    </main>
   );
 }
