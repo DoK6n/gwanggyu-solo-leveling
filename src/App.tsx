2c49d075e0679d9a815952e7db835c4b91bfbc9b
Dok6n <ehrbs2018@gmail.com>
2022-08-29 14:52:16 +0900

    feat: add more data display button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Screen, PlayerInfo, PlayerCall, Category, PlayerHp, Image } from './components';
+import { Screen, PlayerInfo, PlayerCall, Category, PlayerHp, Image, Button } from './components';
 import { currentMonthPlayerDB, totalOperatingRevenue, playerLevel } from './database';
 
 export default function App() {
+  const [isShowMore, setIsShowMore] = useState<boolean>(false);
   return (
     <main className="container min-h-screen max-w-full p-1">
       <section className="flex flex-col items-center p-4 m-2">
@@ -15,15 +16,37 @@ export default function App() {
           <hr className="my-4" />
           <PlayerHp totalOperatingRevenue={totalOperatingRevenue} />
           <hr className="my-4" />
-          {currentMonthPlayerDB.map(db => (
-            <PlayerCall
-              commission={db.commission}
-              date={db.date}
-              downPayment={db.downPayment}
-              operatingRevenue={db.operatingRevenue}
-              key={db.id}
-            />
-          ))}
+          {!isShowMore ? (
+            <React.Fragment>
+              <PlayerCall
+                commission={currentMonthPlayerDB[0].commission}
+                date={currentMonthPlayerDB[0].date}
+                downPayment={currentMonthPlayerDB[0].downPayment}
+                operatingRevenue={currentMonthPlayerDB[0].operatingRevenue}
+                key={currentMonthPlayerDB[0].id}
+              />
+              <PlayerCall
+                commission={currentMonthPlayerDB[1].commission}
+                date={currentMonthPlayerDB[1].date}
+                downPayment={currentMonthPlayerDB[1].downPayment}
+                operatingRevenue={currentMonthPlayerDB[1].operatingRevenue}
+                key={currentMonthPlayerDB[1].id}
+              />
+            </React.Fragment>
+          ) : (
+            currentMonthPlayerDB.map(db => (
+              <PlayerCall
+                commission={db.commission}
+                date={db.date}
+                downPayment={db.downPayment}
+                operatingRevenue={db.operatingRevenue}
+                key={db.id}
+              />
+            ))
+          )}
+          <Button isShowMore={isShowMore} setIsShowMore={setIsShowMore}>
+            {isShowMore ? '접기' : '더 보기'}
+          </Button>
         </Screen>
       </section>
     </main>
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
-export const Button = () => {
+interface ButtonProps {
+  setIsShowMore: React.Dispatch<React.SetStateAction<boolean>>;
+  isShowMore: boolean;
+}
+
+export const Button = ({ children, setIsShowMore, isShowMore }: PropsWithChildren<ButtonProps>) => {
   return (
-    <button className="basis-1/2 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-teal-600 hover:bg-teal-800 active:bg-teal-900">
-      Solo Leveling
+    <button
+      className="basis-1/2 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-teal-600 hover:bg-teal-800 active:bg-teal-900"
+      onClick={() => {
+        setIsShowMore(!isShowMore);
+      }}
+    >
+      {children}
     </button>
   );
 };
