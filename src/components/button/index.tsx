22c8ad2d960510caccd5cf9fa149503c52247f95
Dok6n <ehrbs2018@gmail.com>
2022-08-29 10:48:57 +0900

    design: add button

---
+++ src/components/button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Button = () => {
+  return (
+    <button className="basis-1/2 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-teal-600 hover:bg-teal-800 active:bg-teal-900">
+      Solo Leveling
+    </button>
+  );
+};
