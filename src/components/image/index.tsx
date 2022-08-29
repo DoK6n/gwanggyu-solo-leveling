96c0119e34b52c2896460afbd2cef33edc30ee60
Dok6n <ehrbs2018@gmail.com>
2022-08-29 14:19:31 +0900

    feat: delete useState in image component

--- src/components/image/index.tsx
+++ src/components/image/index.tsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
 import { useLvImageFinder } from '../../hooks/useLvImageFinder';
-import { playerE, playerB } from '../../static/images';
 
 interface ImageProps {
   level: string;
 }
 
 export const Image = ({ level }: ImageProps) => {
-  const [playerImg, setPlayerImg] = useState<string | undefined>(useLvImageFinder(level));
   return (
     <div className="max-w-full text-center">
-      <img src={playerImg} className="max-w-lg max-h-96 w-4/5 m-auto" />
+      <img src={useLvImageFinder(level)} className="max-w-lg max-h-96 w-4/5 m-auto" />
     </div>
   );
 };
