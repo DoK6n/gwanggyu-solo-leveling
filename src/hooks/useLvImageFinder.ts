8a6bfc5bc7efb8322f7a97126c82fb666250af6a
Dok6n <ehrbs2018@gmail.com>
2022-08-29 10:51:35 +0900

    feat: add find player level image hook

---
+++ src/hooks/useLvImageFinder.ts
@@ -0,0 +1,34 @@
+import { LEVEL_CRITERIA } from '../constants/levelcriteria';
+import {
+  playerF,
+  playerFF,
+  playerE,
+  playerD,
+  playerC,
+  playerB,
+  playerA,
+  playerS,
+  playerSS,
+} from '../static/images';
+export const useLvImageFinder = (level: string) => {
+  switch (level) {
+    case LEVEL_CRITERIA.F:
+      return playerF;
+    case LEVEL_CRITERIA['F+']:
+      return playerFF;
+    case LEVEL_CRITERIA.E || LEVEL_CRITERIA['E+']:
+      return playerE;
+    case LEVEL_CRITERIA.D || LEVEL_CRITERIA['D+']:
+      return playerD;
+    case LEVEL_CRITERIA.C || LEVEL_CRITERIA['C+']:
+      return playerC;
+    case LEVEL_CRITERIA.B || LEVEL_CRITERIA['B+']:
+      return playerB;
+    case LEVEL_CRITERIA.A || LEVEL_CRITERIA['A+']:
+      return playerA;
+    case LEVEL_CRITERIA.S:
+      return playerS;
+    case LEVEL_CRITERIA.SS || LEVEL_CRITERIA.SSS:
+      return playerSS;
+  }
+};
