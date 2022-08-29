8bbb002698729de14f60209eef86b015addf0aea
Dok6n <ehrbs2018@gmail.com>
2022-08-29 14:22:11 +0900

    feat: add calculated player db value export

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,14 @@
+import dayjs from 'dayjs';
+import { useLevelFinder, useTotalOperatingRevenue } from '../hooks';
 import db from './db.json';
 import { PlayerDB } from './types';
 
 export const playerDB: PlayerDB[] = db;
+
+export const currentMonthPlayerDB: PlayerDB[] = db
+  .sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf())
+  .filter(db => dayjs(db.date).month() + 1 === dayjs().month() + 1);
+
+export const totalOperatingRevenue = useTotalOperatingRevenue(currentMonthPlayerDB);
+
+export const playerLevel = String(useLevelFinder(useTotalOperatingRevenue(currentMonthPlayerDB)));
